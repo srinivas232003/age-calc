dfb3ab41e517667bafae07b3d5a980ef722fb50b
sharavak <86647111+sharavak@users.noreply.github.com>
2021-12-25 17:01:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let b=document.querySelector("button");
 let ans=document.querySelector('p')
  a=new Date();
 function age(){
-  let year=Math.abs(parseInt(y.value)-a.getYear());
+  let year=Math.abs(parseInt(y.value)-a.getFullYear());
   let month=Math.abs(parseInt(m.value)-a.getMonth());
   let days=Math.abs(parseInt(d.value)-a.getDate());
   ans.innerText=`You are ${year}years ${month}months ${days}days old`;
